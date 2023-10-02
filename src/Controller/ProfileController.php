c703d81a5e30b43bb48296f0b5f0783b4c840533
3eflite <zakariateurki@outlook.com>
2023-10-02 15:20:10 +0200

    Diouf commit

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/profil', name: 'app_profile')]
+class ProfileController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'profil de l\'utilisateur',
+        ]);
+    }
+    #[Route('/commande', name: 'app_commande')]
+    public function index(): Response
+    {
+        return $this->render('commande/index.html.twig', [
+            'controller_name' => 'CommandeController',
+        ]);
+    }
+}
--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -6,10 +6,12 @@ use App\Repository\CategorieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Trait\SlugTrait;
 
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
 class Categorie
 {
+    use SlugTrait;
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
